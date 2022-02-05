ac256ced0a7bb65d23fac6ff275b877b4b84d1d8
Loke Lindberg Nilsson <66024236+Lokecrafter@users.noreply.github.com>
2022-02-05 17:42:56 +0100

    added two new algorithms

--- Program.cs
+++ Program.cs
@@ -6,16 +6,21 @@ namespace Sorting_algorithms
 {
     class Program
     {
+        const int globalArrayLength = 10;
+
+
         static void Main(string[] args)
         {
             Stopwatch stopwatch = new Stopwatch();
  
+            int[] array = GenerateUnsorte§dArray(10, 0, 10);
+            PrintArray(array);
+            //array = BubbleSort(array);
+            //array = SelectionSort(array);
             stopwatch.Start();
-            int[] array = GenerateUnsortedArray(10, 0, 10);
-            PrintArray(array);
-            array = BubbleSort(array);
-            PrintArray(array);
+            array = InsertionSort(array);
             stopwatch.Stop();
+            PrintArray(array);
     
             Console.WriteLine("Elapsed Time is {0} ms", stopwatch.ElapsedMilliseconds);
         }
@@ -41,25 +46,50 @@ namespace Sorting_algorithms
             Console.WriteLine("");
         }
 
-        static void Swap(ref int a, ref int b){
-            /*
+        static void Swap(ref int a, ref int b){  
             a = a+b;
             b = a-b;
             a = a-b;
-            */
-            int temo = a;
-            a= b;
-            b = temo;
         }
 
         static int[] BubbleSort(int[] array){
             for(int i = 0; i < array.Length; i++){
-                for(int j = 0; j < array.Length - i - 1; i++){
-                    if(array[i] > array[i+1]){
-                        Swap(ref array[i], ref array[i+1]);
+                for(int j = 0; j < array.Length - i - 1; j++){
+                    if(array[j] > array[j+1]){
+                        Swap(ref array[j], ref array[j+1]);
                     }
                 }
             }
+            return array;
+        }
+        //Delar upp arrayen i en sorterad och en osorterad del. 
+        //Genom att jämföra array[j] mot varje element i arrayen efter j så kan man byta plats på array[j] och array[j + (x-steg)] om array[j + (x-steg)] är mindre än array[j]
+        static int[] SelectionSort(int[] array){   
+            for(int j = 0; j < array.Length - 1; j++){
+                for(int i = 1 + j; i < array.Length; i++){
+                    if(array[j] > array[i]){
+                        Swap(ref array[j], ref array[i]);
+                    }
+                }
+            }
+            return array;
+        }
+        static int[] InsertionSort(int[] array){
+            //Starta på andra elementet 
+            for(int j = 1; j < array.Length; j++){
+                //Loopa genom föregående element
+                for(int i = 0; i < j; i++){
+                    if(array[j] < array[i]){
+                        //Flytta elementet på j till rätt plats och flytta fram de andra elementen i arrayen
+                        for(int x = i; x < j; x++){
+                            Swap(ref array[j], ref array[x]);
+                        }
+                    }
+                }
+            }
+            return array;
+        }
+        static int[] MergeSort(int[] array){
             return array;
         }
     }
